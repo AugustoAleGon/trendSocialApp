b46b53ef6dd0054619cbe330e34a78e24f987cac
augustoalegon <mac@MacBook-Pro-de-Mac.local>
2018-04-27 01:57:08 -0500

    Set name on the Reactotron configuration

--- App/Config/ReactotronConfig.js
+++ App/Config/ReactotronConfig.js
@@ -8,7 +8,7 @@ import sagaPlugin from 'reactotron-redux-saga'
 if (Config.useReactotron) {
   // https://github.com/infinitered/reactotron for more options!
   Reactotron
-    .configure({ name: 'DogApp',
+    .configure({ name: 'trendSocialApp',
       socketIoProperties: {
         reconnection: true,
         reconnectionDelay: 2000,
